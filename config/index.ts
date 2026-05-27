2f867308b52aaeba83f391721be7add8055409fc
gsy <1286195889@qq.com>
2026-05-27 11:22:36 +0800

    修复样式

--- config/index.ts
+++ config/index.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    favicon: '/static/favicon.svg', 
+    favicon: '/static/favicon.png', 
     postcss: {
       autoprefixer: {
         enable: true,
